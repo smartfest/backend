165a7733222865c5fef22c6aede1acdb86d12043
Dante Giuliano <dante.giuliano@est.fi.uncoma.edu.ar>
2022-11-23 13:23:02 -0300

    changes

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 const express = require('express')
 const bodyParser=require('body-parser')
 
-
 const app=express()
 const api_route= require ('./routes/evento.js')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+
+app.get('/api' ,function (req,res){
+    res.send({status:"Smart FEST ON"}).status(200)
+})
 
 app.use('/api',api_route)
 module.exports= app
--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports={
     port:process.env.PORT || 3003,
-    db:process.env.MONGODB || 'mongodb://127.0.0.1:27017/smartfest',
+    db:process.env.MONGODB || 'mongodb://uncx3n8zkfviik7pjo61:qvJkwAhNlnBvJZPlRifb@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bgh6smneuv7tpor?replicaSet=rs0',
     SECRET_TOKEN:'laclavedeTokensdeLuisGarcia'
 }
+
+//db:process.env.MONGODB || 'mongodb://uncx3n8zkfviik7pjo61:qvJkwAhNlnBvJZPlRifb@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bgh6smneuv7tpor?replicaSet=rs0',
