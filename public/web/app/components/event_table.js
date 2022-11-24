ba12fed16bbdc7e25459cdf252df04009d9ff3fb
dante <dante.giuliano@est.fi.uncoma.edu.ar>
2022-11-24 17:29:00 -0300

    avances en modal view

--- public/web/app/components/event_table.js
+++ public/web/app/components/event_table.js
@@ -10,7 +10,7 @@
     <td>${evento.titulo}</td>
     <td>${fecha} Horario: ${horario}</td>
     <td class="buttons">
-        <button type="button" class="btn btn-primary"><img src="../../assets/icons/view.png" alt="" srcset="" srcset="" width="36px" height="36px"></button>
+        <button type="button" onclick="view_event('${id}')" class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#modal_view"><img src="../../assets/icons/view.png" alt="" srcset="" srcset="" width="36px" height="36px"></button>
         <button type="button" onclick="edit_event('${id}')" class="btn btn-warning " data-bs-toggle="modal" data-bs-target="#modalId"><img src="../../assets/icons/edit.png" alt="" sizes="" srcset="" width="36px" height="36px"></button>
         <button type="button" class="btn btn-danger" onclick="delete_event('${id}')"><img src="../../assets/icons/Delete.svg" alt="" sizes="" srcset="" width="36px" height="36px"></button>
     </td>
